ebea2db432c5806716954bc09f55fac2068c82b9
Jakob Rath <git@jakobrath.eu>
2025-03-23 16:26:37 +0100

    Extract NixOS module into separate file

--- flake.nix
+++ flake.nix
@@ -1,4 +1,5 @@
 {
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     crane.url = "github:ipetkov/crane";
@@ -69,82 +70,7 @@
         inherit scree;
       };
 
-      nixosModules.default = { config, lib, pkgs, ...}:
-      with lib;
-      let
-        cfg = config.jr.services.scree;
-      in
-      {
-        options.jr.services.scree = {
-          enable = mkEnableOption "Scree service";
-
-          database = mkOption {
-            type = types.str;
-            example = "host=/run/postgresql dbname=scree";
-            description = "Database connection string. Note that the database must be configured separately.";
-          };
-
-          listen = mkOption {
-            type = types.str;
-            default = "127.0.0.1:3000";
-            example = "/run/scree/scree.sock";
-            description = "Socket address on which the service listens to HTTP connections (either IP:Port for a regular socket or an absolute path for a unix socket).";
-          };
-
-          setRealIp = mkOption {
-            type = types.bool;
-            default = false;
-            description = "Obtain the client's remote IP address from the header 'x-real-ip'. Useful if connections go through a reverse proxy.";
-          };
-
-          user = mkOption {
-            type = types.str;
-            default = "scree";
-            description = "User account under which the service runs.";
-          };
-
-          group = mkOption {
-            type = types.str;
-            default = "scree";
-            description = "Group account under which the service runs.";
-          };
-
-          package = mkOption {
-            type = types.package;
-            default = self.packages.${pkgs.system}.default;
-            description = "The package to use for this service.";
-          };
-        };
-
-        config = mkIf cfg.enable {
-
-          users.users = optionalAttrs (cfg.user == "scree") {
-            scree = {
-              group = cfg.group;
-              isSystemUser = true;
-            };
-          };
-
-          users.groups = optionalAttrs (cfg.group == "scree") {
-            scree = {};
-          };
-
-          systemd.services.scree = {
-            description = "Scree task monitoring";
-            wantedBy = [ "multi-user.target" ];
-
-            serviceConfig = {
-              User = cfg.user;
-              Group = cfg.group;
-              ExecStart =
-                "${cfg.package}/bin/scree --db ${escapeShellArg cfg.database} run " +
-                "--listen ${escapeShellArg cfg.listen} " +
-                (if cfg.setRealIp then "--set-real-ip " else "");
-            };
-          };
-
-        };
-      };
+      nixosModules.default = (import ./module.nix) { flake = self; };
 
       devShells.default = craneLib.devShell {
         # Inherit inputs from checks.
@@ -157,5 +83,8 @@
         packages = with pkgs; [
         ];
       };
-    });
+
+    }
+  );
+
 }
---
+++ module.nix
@@ -0,0 +1,79 @@
+{ flake }:
+{ config, lib, pkgs, ...}:
+with lib;
+let
+  cfg = config.jr.services.scree;
+in
+{
+  options.jr.services.scree = {
+
+    enable = mkEnableOption "Scree service";
+
+    database = mkOption {
+      type = types.str;
+      example = "host=/run/postgresql dbname=scree";
+      description = "Database connection string. Note that the database must be configured separately.";
+    };
+
+    listen = mkOption {
+      type = types.str;
+      default = "127.0.0.1:3000";
+      example = "/run/scree/scree.sock";
+      description = "Socket address on which the service listens to HTTP connections (either IP:Port for a regular socket or an absolute path for a unix socket).";
+    };
+
+    setRealIp = mkOption {
+      type = types.bool;
+      default = false;
+      description = "Obtain the client's remote IP address from the header 'x-real-ip'. Useful if connections go through a reverse proxy.";
+    };
+
+    user = mkOption {
+      type = types.str;
+      default = "scree";
+      description = "User account under which the service runs.";
+    };
+
+    group = mkOption {
+      type = types.str;
+      default = "scree";
+      description = "Group account under which the service runs.";
+    };
+
+    package = mkOption {
+      type = types.package;
+      default = flake.packages.${pkgs.system}.default;
+      description = "The package to use for this service.";
+    };
+
+  };
+
+  config = mkIf cfg.enable {
+
+    users.users = optionalAttrs (cfg.user == "scree") {
+      scree = {
+        group = cfg.group;
+        isSystemUser = true;
+      };
+    };
+
+    users.groups = optionalAttrs (cfg.group == "scree") {
+      scree = {};
+    };
+
+    systemd.services.scree = {
+      description = "Scree task monitoring";
+      wantedBy = [ "multi-user.target" ];
+
+      serviceConfig = {
+        User = cfg.user;
+        Group = cfg.group;
+        ExecStart =
+          "${cfg.package}/bin/scree --db ${escapeShellArg cfg.database} run " +
+          "--listen ${escapeShellArg cfg.listen} " +
+          (if cfg.setRealIp then "--set-real-ip " else "");
+      };
+    };
+
+  };
+}
